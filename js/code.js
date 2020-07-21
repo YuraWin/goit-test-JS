d36a6e7f8621a0470d47752968e6078357719470
YuraWin <yurainw@ukr.net>
2020-07-21 17:53:13 +0300

    Work1

--- js/code.js
+++ js/code.js
@@ -1,4 +1,25 @@
-"use strict";
+'use strict';
 // console.log(5 && false && "hello");
 
-console.log(0 || 8 || true);
+// console.log(0 || 8 || true);
+// alert('before');
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('Введите значение');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '89.231px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+// console.log(elementWidth);
+
+// const result2 = Number.parseFloat(elementWidth);
+// console.log(result2);
+
+// const salary = 1258.256687;
+// console.log(Number(salary.toFixed(3)));
+// console.log(salary.toFixed());
