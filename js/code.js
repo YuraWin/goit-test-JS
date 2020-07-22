c9a4915034ab396851ae80c5b0f503e051fbd553
YuraWin <yurainw@ukr.net>
2020-07-22 17:36:43 +0300

    work1

--- js/code.js
+++ js/code.js
@@ -20,6 +20,126 @@
 // const result2 = Number.parseFloat(elementWidth);
 // console.log(result2);
 
-// const salary = 1258.256687;
+// const salary = 1258.256587;
 // console.log(Number(salary.toFixed(3)));
 // console.log(salary.toFixed());
+// *******************
+// const base = 3;
+// const power = 3;
+
+// console.log(Math.pow(base, power));
+
+// console.log(2 ** 2);
+// console.log(Math.sqrt(9));
+// ****************************
+// Example
+
+// let base = prompt('Давай число');
+// base = Number(base);
+// console.log('Число', base);
+
+// let power = prompt('Давай степень');
+// power = Number(power);
+// console.log('Степень', power);
+
+// const result = base ** power;
+// console.log('Результат', result);
+
+// *****************************
+
+// const min = 10;
+// const max = 30;
+// let result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
+
+// ***********************************
+// const colors = ['tomato', 'teal', 'black', 'green', 'blue'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+// console.log(color);
+// document.body.style.backgroundColor = color;
+
+// **********************************************
+// const message = 'Привет всем  джунам !!!';
+// console.log(message.length);
+
+// const firstName = 'Yuriy';
+// const lastName = 'Homenko';
+// const room = 34;
+// const fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// console.log(`Гость ${fullName} поселяетcя в комнате ${room}`);
+
+// ***************************************************
+// let brand = 'SaMsUnG';
+// console.log(brand);
+// brand = brand[0] + brand.slice(1).toLowerCase();
+// console.log(brand);
+// ******************************************************
+
+// const blackWord1 = 'спам';
+
+// const string1 = 'Сегодня нам попался Спам и это опастность.';
+// console.log(string1.toLowerCase().includes(blackWord1));
+// let result = string1.toLowerCase().length;
+// console.log(result);
+// **********************************************************
+
+// console.log(0 || false || '' || undefined);
+
+//**************************************** */
+// const x1 = 10;
+// const x2 = 25;
+// const number = 15;
+
+// console.log(`Число ${number} попадает в отрезок до ${x1}`, number < x1);
+
+// console.log(`Число ${number} попадает в отрезок больше ${x2}`, number > x1);
+
+// console.log(
+//   `Число ${number} попадает в отрезок больше ${x1} и меньше ${x2}`,
+//   number > x1 && number < x2,
+// );
+//******************************************* */
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log('Можно открыть чат?', canOpenChat);
+
+//******************************************** */
+
+// const balance = 100;
+// let message;
+// let message2;
+
+// if (balance >= 0) {
+//   message = 'Позитивный баланс';
+// } else {
+//   message = 'Негативный баланс';
+// }
+// console.log(message);
+
+// console.log((message = balance > 0 ? 'Позитив' : 'Негатив'));
+
+// *****Задача 1********************************************
+
+let balance = 10000;
+const payment = 2000;
+
+console.log(
+  `Общая стоимость заказа ${payment}  кредитов. Проверяем кол-во доступных средств на счету`,
+);
+if (payment <= balance) {
+  balance = balance - payment;
+  console.log(`Насчету осталось ${balance} кредитов`);
+} else {
+  console.log(`На счету недостаточно средств для проведения операции!`);
+}
+console.log('Операция завершена');
