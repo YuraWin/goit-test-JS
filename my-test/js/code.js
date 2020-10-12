dca216d91f180262b3f02cd7bdd167ed128d22f2
YuraWin <yurainw@ukr.net>
2020-10-12 17:53:25 +0300

    comit

--- my-test/js/code.js
+++ my-test/js/code.js
@@ -3277,6 +3277,22 @@
 // console.log(source);
 
 // 28****************************************************
+const output = document.querySelector('#js-output');
+const input = document.querySelector('#js-input');
+const btn = document.querySelector('#js-btn');
+
+const updateView = () => {
+  output.textContent = localStorage.getItem('goit-js-module-12' || '');
+};
+
+const handleClick = () => {
+  localStorage.setItem('goit-js-module-12', input.value);
+  updateView();
+};
+
+updateView();
+
+btn.addEventListener('click', handleClick);
 
 // ****************************************************
 // ****************************************************
